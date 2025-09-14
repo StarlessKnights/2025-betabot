e85ab57ecc5ada8fc922f9e0ee6836c85e0f354b
StarlessKnights <StarlessKnights@users.noreply.github.com>
2025-09-14 11:00:41 -0500

    feat: match time display and april tags

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -11,6 +11,7 @@ import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -117,5 +118,6 @@ public class DriveSubsystem extends SubsystemBase {
     field.setRobotPose(poseEstimator.getPose2d());
 
     SmartDashboard.putData("field", field);
+    SmartDashboard.putNumber("Match Time", DriverStation.getMatchTime());
   }
 }
